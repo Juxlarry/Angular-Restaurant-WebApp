2b8b1b06d28f44ae50d84d74684c5fcd19b6b6ff
Lawrence D <60719231+Juxlarry@users.noreply.github.com>
2020-09-05 17:41:11 -0700

    Angualr 10 Restaurant App built to the add Header and Footer to App components

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service'; 
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+ 
+  dishes: Dish[]; 
+
+  selectedDish: Dish;
+
+  constructor( private dishSerivice: DishService) { 
+
+  }
+
+  ngOnInit(){
+    this.dishes = this.dishSerivice.getDishes();
+  }
+
+  onSelect(dish: Dish){
+   this.selectedDish = dish; 
+  }
+
+}
