326f8b440b748c2f5679e7ec0475e1eca9f5d73b
Lawrence D <60719231+Juxlarry@users.noreply.github.com>
2020-09-06 00:45:29 -0700

    Angular-routing basics where I have added routes for components on my page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component'; 
 
 import { DishService } from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'; 
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,11 +30,14 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
+    AppRoutingModule,
     BrowserAnimationsModule, 
     MatToolbarModule, 
     FlexLayoutModule, 
