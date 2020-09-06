86ad359fc1342612570998c28a95b155ae443f9a
Lawrence D <60719231+Juxlarry@users.noreply.github.com>
2020-09-06 01:51:54 -0700

    Added some Single Page App features to the angular App

---
+++ src/app/services/promotion.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Promotion } from '../shared/promotion'; 
+import { PROMOTIONS } from '../shared/promotions';
+@Injectable({
+  providedIn: 'root'
+})
+export class PromotionService {
+
+  constructor() { }
+
+  getPromtions(): Promotion[] {
+    return PROMOTIONS; 
+  }  
+
+  getPromotion(id:string): Promotion {
+    return PROMOTIONS.filter((dish) =>( 
+      dish.id ===id))
+    [0];
+  }
+
+  getFeaturedPromotion(): Promotion {
+    return PROMOTIONS.filter((promo) => promo.featured)[0];
+  }
+}
